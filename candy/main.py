b36b95dcb34f3daa7ee14a8699dc6c818902c0b3
node.dom <dominique.reese444@gmail.com>
2024-02-24 10:49:49 -0500

    Attempt 2

--- candy/main.py
+++ candy/main.py
@@ -20,42 +20,67 @@ class TestSolution(unittest.TestCase):
         actual = candy(ratings)
         expected = 5
         self.assertEqual(expected, actual)
+
     def test_two(self):
         ratings = [1, 2, 2]
         actual = candy(ratings)
         expected = 4
         self.assertEqual(expected, actual)
 
+    def test_three(self):
+        ratings = [1, 1, 1]
+        actual = candy(ratings)
+        expected = 3
+        self.assertEqual(expected, actual)
+
+    def test_four(self):
+        ratings = [1]
+        actual = candy(ratings)
+        expected = 1
+        self.assertEqual(expected, actual)
+
+    def test_five(self):
+        ratings = []
+        actual = candy(ratings)
+        expected = 0
+        self.assertEqual(expected, actual)
+
+    def test_six(self):
+        ratings = [1, 3, 2, 2, 1]
+        actual = candy(ratings)
+        expected = 7
+        self.assertEqual(expected, actual)
+
 
 def candy(ratings: List[int]) -> int:
-    # If ratings array countains only less than 2 elements
+    # If ratings array contains less than 2 elements
     # return the length of the array
     if len(ratings) < 2:
         return len(ratings)
 
+    candy_list = [1] * len(ratings)
     idx = 0
-    count = 1
-    prev_count = 1
     runner_idx = 1
     while runner_idx <= len(ratings) - 1:
         if ratings[idx] < ratings[runner_idx]:
-            prev_count += 1
-            count += prev_count
+            candy_list[runner_idx] = candy_list[idx] + 1
+            idx = runner_idx
+            runner_idx += 1
         elif ratings[idx] == ratings[runner_idx]:
-            prev_count = 1
-            count += prev_count
-        else:  # Case where ratings[idx] > ratings[runner_idx]
-            desc_count = 1
-            while runner_idx < len(ratings) - 1 and ratings[runner_idx] > ratings[runner_idx + 1]:
-                count += desc_count
-                desc_count += 1
+            candy_list[runner_idx] = 1
+            idx = runner_idx
+            runner_idx += 1
+        else:
+            x = idx
+            while runner_idx <= len(ratings) - 1 and ratings[runner_idx] < ratings[x]:
+                for i in range(idx, runner_idx):
+                    candy_list[i] += 1
+                x = runner_idx
                 runner_idx += 1
-            count += desc_count
-            prev_count = 1
-        idx = runner_idx
-        runner_idx += 1
-    count += 1
-    return count
+            idx = x
+
+    total = sum(candy_list)
+    return total
 
 
 if __name__ == "__main__":
